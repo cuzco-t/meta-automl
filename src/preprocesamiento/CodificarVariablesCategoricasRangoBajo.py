86b44558e167499a9860d15a1a5646955213327a
Cuzco <jonnathan.cuzcot@ucuenca.edu.ec>
2026-02-14 23:49:36 -0500

    Codificar Variables Categoricas Rango Bajo refactorizado

--- src/preprocesamiento/CodificarVariablesCategoricasRangoBajo.py
+++ src/preprocesamiento/CodificarVariablesCategoricasRangoBajo.py
@@ -12,18 +12,18 @@ class CodificarVariablesCategoricasRangoBajo(BaseEstimator, TransformerMixin, Re
             cls._instance = super(CodificarVariablesCategoricasRangoBajo, cls).__new__(cls)
         return cls._instance
 
-    def __init__(self, permitir_none=True, random_state=None):
+    def __init__(self, permitir_none=True, semilla=None, config_test=None):
         """
         permitir_none: si True, permite que no se aplique ninguna técnica
-        random_state: para reproducibilidad
+        semilla: para reproducibilidad
         """
         # Evitamos re-inicializar si la instancia ya existe
         if not hasattr(self, "_initialized"):
-            self.log_fase = "codificar_variables_categoricas_rango_bajo"
+            RegistroTecnica.__init__(self, log_fase="codificar_variables_categoricas_rango_bajo")
             self.permitir_none = permitir_none
-            self.random_state = random_state
-            self.log_algoritmo = None
-            self.log_params = {}
+            self.semilla = semilla
+            self.config_test = config_test
+            self.reiniciar()
             self._initialized = True
     
     def reiniciar(self):
@@ -39,70 +39,96 @@ class CodificarVariablesCategoricasRangoBajo(BaseEstimator, TransformerMixin, Re
             return [t for t in tecnicas if t is not None]
         return tecnicas
 
-    def fit(self, X, y=None):
+    def fit(self, X: pd.DataFrame, y: pd.Series = None):
         """
-        Selecciona aleatoriamente la técnica a aplicar a las variables categóricas
-        de bajo rango y la guarda en self.log_algoritmo
+        Decide aleatoriamente la técnica a aplicar y la guarda en self.log_algoritmo
         """
         if self.log_algoritmo is not None:
             return self
         
-        generador_aleatorio = np.random.default_rng(self.random_state)
-        TECNICAS = [None, "one-hot-encoding", "label-encoding"]
-        TECNICAS = self._permitir_none(TECNICAS)
+        if self.config_test is not None:
+            self.log_algoritmo = self.config_test.get("algoritmo")
+            self.log_params = self.config_test.get("params")
 
-        self.log_algoritmo = generador_aleatorio.choice(TECNICAS)
-        return self
-
-    def transform(self, X, y=None):
-        """
-        Aplica la codificación seleccionada a las variables categóricas de bajo rango.
-        También elimina columnas con ratio de valores únicos >= 0.90
-        """
-        if self.log_algoritmo is None:
-            self.registrar_tecnica(self.log_fase, self.log_algoritmo, self.log_params)
-            return X if y is None else (X, y)
-
-        X_df = X.copy() if isinstance(X, pd.DataFrame) else pd.DataFrame(X)
-
-        columnas_one_hot = []
-        for col in X_df.columns:
-            if pd.api.types.is_object_dtype(X_df[col]):
-                ratio_unicos = X_df[col].nunique() / len(X_df)
-
-                # Dataset de entrenamiento
-                if col not in self.log_params:
-                    # Columnas de bajo rango
-                    if ratio_unicos <= 0.05:
-                        if self.log_algoritmo == "one-hot-encoding":
-                            dummies = pd.get_dummies(X_df[col], prefix=col, dtype=int)
-                            X_df = pd.concat([X_df.drop(columns=[col]), dummies], axis=1)
-                            columnas_one_hot.append(col)
-
-                        elif self.log_algoritmo == "label-encoding":
-                            # Crea el mapa de categorías a números
-                            categorias = sorted(X_df[col].dropna().unique())
-                            mapa = {cat: i for i, cat in enumerate(categorias)}
-                            self.log_params[col] = mapa
-                            
-                            self.registrar_tecnica(self.log_fase, self.log_algoritmo, self.log_params)
-                            X_df[col] = X_df[col].map(self.log_params[col])
-
-                # Dataset de validacion
-                else:
-                    if self.log_algoritmo == "one-hot-encoding":
-                        dummies = pd.get_dummies(X_df[col], prefix=col, dtype=int)
-                        X_df = pd.concat([X_df.drop(columns=[col]), dummies], axis=1)
-
-                    elif self.log_algoritmo == "label-encoding":
-                        X_df[col] = X_df[col].map(self.log_params[col])
-
-        if self.log_algoritmo == "one-hot-encoding":
-            self.log_params = columnas_one_hot
-            self.registrar_tecnica(self.log_fase, self.log_algoritmo, self.log_params)
-
-
-        if y is None:
-            return X_df
         else:
-            return X_df, y
+            generador_aleatorio = np.random.default_rng()
+            TECNICAS = self._permitir_none([
+                None, 
+                "one_hot_encoding", 
+                "label_encoding"
+            ])
+            self.log_algoritmo = generador_aleatorio.choice(TECNICAS)
+            
+            self.registrar_algoritmo(self.log_algoritmo)
+            self._calcular_parametros(X)
+
+        self.registrar_algoritmo(self.log_algoritmo)
+        return self
+    
+    def transform(self, X: pd.DataFrame, y: pd.Series = None):
+        """
+        Aplica la técnica de codificación seleccionada a las columnas categóricas
+        de bajo rango (<=5% categorías únicas)
+        """
+        match self.log_algoritmo:
+            case None:
+                return X
+            
+            case "one_hot_encoding":
+                X_codificado = self._codificar_one_hot_encoding(X.copy())
+                return X_codificado
+            
+            case "label_encoding":
+                X_codificado = self._codificar_label_encoding(X.copy())
+                return X_codificado
+            
+            case _:
+                raise ValueError(f"Técnica de codificación no soportada: {self.log_algoritmo}")
+        
+    def _calcular_parametros(self, X: pd.DataFrame) -> None:
+        """
+        Calcula y guarda en self.log_params los parámetros necesarios para la técnica seleccionada
+        """
+        for col in X.columns:
+            if not pd.api.types.is_object_dtype(X[col]):
+                continue
+
+            ratio_unicos = X[col].nunique() / len(X)
+
+            if ratio_unicos <= 0.05:
+                if self.log_algoritmo == "one_hot_encoding":
+                    self.log_params[col] = True
+                    continue
+
+                elif self.log_algoritmo == "label_encoding":
+                    categorias = sorted(X[col].dropna().unique())
+                    mapa = {cat: i for i, cat in enumerate(categorias)}
+                    self.log_params[col] = mapa
+
+            self.registrar_parametros(self.log_params)
+
+    def _codificar_one_hot_encoding(self, X_copy: pd.DataFrame) -> pd.DataFrame:
+        """
+        Aplica one hot encoding a las columnas categóricas de bajo rango codificadas en self.log_params
+        
+        :return: DataFrame con las columnas categóricas de bajo rango codificadas
+        :rtype: DataFrame
+        """
+        for col in self.log_params.keys():
+            dummies = pd.get_dummies(X_copy[col], prefix=col, dtype=int)
+            X_copy = pd.concat([X_copy.drop(columns=[col]), dummies], axis=1)
+
+        return X_copy
+    
+    def _codificar_label_encoding(self, X_copy: pd.DataFrame) -> pd.DataFrame:
+        """
+        Aplica label encoding a las columnas categóricas de bajo rango codificadas en self.log_params
+        
+        :return: DataFrame con las columnas categóricas de bajo rango codificadas
+        :rtype: DataFrame
+        """
+        for col, mapa in self.log_params.items():
+            if col in X_copy.columns:
+                X_copy[col] = X_copy[col].map(mapa)
+
+        return X_copy
